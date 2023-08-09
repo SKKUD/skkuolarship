349fee7ef3b508ed4b14a5b3bd9c045f7ae35f4c
Hola052 <54920318+JooEun-Jeong@users.noreply.github.com>
2023-08-09 16:45:06 +0900

    feat: fixed the keys of extractor output(json) (#2)
    * extract essential components from given texts
    
    * designate output format as json
    
    * readme file
    
    * fix json's keys & update prompt

--- extractor/extractor.py
+++ extractor/extractor.py
@@ -33,9 +33,18 @@ OPENAI_API_KEY = get_secret("OPENAI_API_KEY")
 summary_schema = {
     "type": "object",
     "properties": {
+        # "신청기간": {
+        #     "type": "array",
+        #     "description": "장학금 신청 시작 날짜와 마감 날짜. '2023년 7월 8일 ~ 2023년 7월 10일 5시까지' 를 시작 날짜, 마감 날짜로 구분하여 number type으로 작성한 array를 반환해줘. 예를 들어, '2022년 1월 1일 ~ 2022년 2월 3일 17시까지'이 입력값으로 들어오면, 출력값은 [20220101, 20220203] 이어야해",
+        #     "items": {
+        #         "type": "number",
+        #         "description": "장학금 신청 시작 날짜와 마감날짜를 숫자로 표현. 2023년 7월 8일이면 20230708을 반환 "
+        #     }
+        # },
         "신청기간": {
             "type": "string",
-            "description": "장학금 신청기간 혹은 장학금 신청마감기한"
+            "description": "장학금 신청 시작 날짜와 마감 날짜. 만약 \.으로 구분이 되어있다면 \.을 년, 월, 일로 구분해줘. 예를 들어, '2023년 7월 8일에서 2023년 7월 10일 오후 5시까지'라면 '2023년 7월 8일 ~ 2023년 7월 10일 17시' 라고 적어줘. 만약 연도가 없이 월, 일만 적혀있다면 '2023년'을 붙여줘. 예를 들어 '7월 8일 ~ 7월 10일'라면 '2023년 7월 8일 ~ 2023년 7월 10일'라고 적어줘."
+
         },
         "선발인원": {
             "type": "string",
@@ -78,7 +87,8 @@ summary_schema = {
             "description": "문의처의 주소, 웹사이트 주소, 번호 혹은 이메일"
         },
     },
-    "required": ["신청기간", "지원대상"]
+    "required": ["신청기간", "지원대상", "선발인원", "장학혜택", "접수방법", "문의"]
+
 }
 
 # 키워드 추출해줘.
@@ -90,32 +100,19 @@ def main(script):
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system",
-             "content": f"""You are a helpful assistant that exactly classify the information from user-given texts into given components. components: '신청기간(혹은 장학금 신청마감기한)', '선발인원', '장학혜택', '접수방법(제출처, 제출서류, 서류제출기한)', '지원대상(혹은 신청자격)', '문의'. """
+             "content": f"""You are a helpful assistant that exactly classify the information from user-given texts into given components. components:
+              1. '신청기간': 장학금 신청하는 기간 또는 마감기한.
+              2. '선발인원': 장학금에서 모집 또는 선발하는 인원 수
+              3. '장학혜택': 기숙사, 거주지, 시급, 등록금, 지원금액 등의 혜택
+              4. '접수방법(제출처, 제출서류, 서류제출기한)': 장학금을 신청하기 위한 방법
+              5. '지원대상(혹은 신청자격)': 장학금에 신청하기 위한 대상과 자격
+              6. '문의': 장학금과 관련해 문의할 내용의 연락처 혹은 홈페이지 주소 """
              },
             {"role": "user", "content": f"user-given texts: {script}"},
-            # {"role": "assistant", "content": """
-            #     <추가 지시사항>
-            #     분류한 글을 아래 다음과 같은 json 형태로 반환해줘.
-            #     Keys must not be changed, but values can be changed.
-            #     Key와 관련된 내용이 없다면 value를 null로 적어줘.
-            #     {{
-            #     "신청기간": "2023년 7월 14일(금) ~ 7월 28일(금) 11시",
-            #     "선발인원": "3명",
-            #     "장학혜택": "학기당 200만원",
-            #     "접수방법": {{
-            #       "제출처": "600주년기념관 1층 학생지원팀",
-            #       "제출서류": ["사회공헌활동 증빙", "재단서식", "기본서류", "모집분야 별 선택요건 증빙"],
-            #       "제출기한": "2023년 7월 14일(금) ~ 7월 28일(금) 11시"
-            #     }},
-            #     "지원대상": ["대한민국 국적의 대학생", "2023학년도 2학기에 재학 예정인 학생",
-            #     "기업 및 타 재단에서 ‘생활비 장학금’을 수혜 받고 있지 않은 학생"],
-            #     "문의": "skkujanghak@skku.edu"
-            #     }}
-            #     """}
         ],
         functions=[{
             "name": "extractor",
-            "description": "extract essential information from user-given texts. Key와 관련된 내용이 없다면 value를 null로 적어줘",
+            "description": "extract essential information from user-given texts. Key와 관련된 내용이 없다면 value를 null type의 null로 적어줘",
             "parameters": summary_schema
         }],
         function_call={
